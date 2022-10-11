f83d4fe57bbfd2d0c7af8149b4efcdb1e780b392
xentoro00 <diorhyseni0@gmail.com>
2022-10-12 01:08:35 +0200

    update

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+const header = document.getElementsByTagName("h1")[0]
+const body = document.getElementsByTagName("p")[0];
+const input = document.getElementById("searchInput")
+const button = document.getElementById("searchButton")
+var data;
 async function main() {
-    const data = fetch("http://localhost:3000/");
-    console.log(data);
+    await fetch('http://localhost:3000/data')
+    .then(response =>  response.json())
+    .then(json =>  data = json);
+header.innerText = data.titleSong;
+body.innerText = data.bodySong;
 }
+
 main();
+button.addEventListener("click", () => {
+    if(input.value == "") return
+    let data = {
+        inputText: input.value.toString()
+    }
+    console.log(JSON.stringify(data));
+    fetch('http://localhost:3000/searchSong', {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+})
